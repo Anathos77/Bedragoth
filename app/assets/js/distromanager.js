30a8ae77ed0156ca2715eabf85f44315f93aca21
Anathos <maximebro1@gmail.com>
2021-02-16 03:43:03 +0100

    .

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://www.mediafire.com/file/o1ovxj43wrsfjdj/distribution.json/file'
+        const distroURL = 'https://github.com/Anathos77/Test/blob/master/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
